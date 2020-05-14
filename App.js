5e45c37d23c4b01dee46b6a3409da7b36fddd0a1
Aaron Nguyen <aaronnguyen.vic@gmail.com>
2020-05-14 13:16:22 +1000

    Use Context, Provider and Reducer to manage data flow

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
 import IndexScreen from './src/screens/IndexScreen';
 import CreateScreen from './src/screens/CreateScreen';
 import EditScreen from './src/screens/EditScreen';
 import ShowScreen from './src/screens/ShowScreen';
+
+import { BlogProvider } from './src/context/BlogContext';
 
 const Stack = createStackNavigator();
 
@@ -14,12 +17,18 @@ const App = () => {
       <Stack.Navigator>
         <Stack.Screen
           name='Index'
-          component={ShowScreen}
-          options={{ title: 'Index' }}
+          component={IndexScreen}
+          options={{ title: 'Blog List' }}
         />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
-export default App;
+export default () => {
+  return (
+    <BlogProvider>
+      <App />
+    </BlogProvider>
+  );
+};
